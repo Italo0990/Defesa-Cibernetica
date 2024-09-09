ad7bc9b1fc83fb4cd35308b8ee4a90bd5c84be6f
Italo0990 <italo09705@gmail.com>
2024-09-09 10:11:57 -0300

    af.py

--- CFS 02-09-24/af.py
+++ CFS 02-09-24/af.py
@@ -1,5 +1,8 @@
-print ('Atividade 2 - Code For Security\n')
-
 raio = float(input('Digite o raio: '))
+PI = 3.14
+comprimento = 2 * PI * raio
 
-c = 2 * 3.14 * raio
+print(f'''
+    o comprimento da circunferencia  com raio {raio:.2f}
+    é de {comprimento:.2f}
+    ''')
