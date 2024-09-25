9e4ff34a73c9279cbd383b5ebc8c3775204c68c0
Italo0990 <italo09705@gmail.com>
2024-09-25 12:58:19 -0300

    Delete CFS - 25-09-25 directory

--- CFS - 25-09-25/ad.py
+++
@@ -1,9 +0,0 @@
-tupla = (1,2,3,)
-tupla2 = (4,5,6)
-
-print(tupla + tupla2)
-print(tupla * 2)
-
-a,b,c = tupla
-print(a)
-print(b)
--- CFS - 25-09-25/aj.py
+++
@@ -1,6 +0,0 @@
-dicionario = {'nome': 'Sarah', 'sobrenome': 'Lima'}
-print(dicionario)
-
-s = dicionario.setdefault('idade', 23)
-print(s)
-
