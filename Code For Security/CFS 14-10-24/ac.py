9902b41ec41d23a5077c0e582aa0e67649a80a47
Italo0990 <italo09705@gmail.com>
2024-10-16 07:26:18 -0300

    ac.py

--- Code For Security/CFS 14-10-24/ac.py
+++ Code For Security/CFS 14-10-24/ac.py
@@ -1,3 +1,17 @@
 # Acesse o arquivo access_log com informações de logs do apache.
 # Exiba uma lista e quantidade de IPS únicos que 
 # acessaram o servidor durante o período 
+
+with open('access_log') as logs:
+    #podemos iterar sobre o logs ou sobre o readlines
+    for log in logs:
+    #for log in logs.readlines():
+        #Agora usamos split para separar os campos do log na linha
+        linha = log.split()
+        #prevenimos erro de linha em branco verificando se ela é uma lista valida
+        if linha:
+            #add a linha ao conjunto
+            ips.add(linha[0])
+
+print(ips)
+print(len(ips)) #para sabermos a quantidade de IPs
